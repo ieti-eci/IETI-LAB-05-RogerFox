7bd042068e3220c9f590bd596a34efa70526b5d2
KyrieLW <james.weisnner@mail.escuelaing.edu.co>
2021-10-07 15:40:52 -0500

    changing directories, creating react application

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return 
+    <main>
+      <form>
+        <input type="text" placeholder="Task name"/>
+        <button>Create Task</button>
+      </form>        
+    </main>;
+  
+}
+
+export default App;
