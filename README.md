ab77ed8edbd9f7a6ffeb5655338f88521807916e
Verbo Camacho V <verbocamacho@gmail.com>
2021-10-07 16:06:55 -0500

    TaskItem

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+# INTEGRANTES
+
+## James Allan Weisnner
+
+## Verbo Julian Camacho Villamarin
+
 ## Available Scripts
 
 In the project directory, you can run:
---
+++ src/TaskItem.js
@@ -0,0 +1,19 @@
+export const TaskItem = ({ isChecked, taskName}) => {
+
+    
+
+    const styleOfTheComponent = {
+        textDecoration: isChecked ? "line-through" : "",
+
+    };
+
+    return (
+        <li>
+            <input cheked={isChecked} type="checkbox"/>
+            <span style={styleOfTheComponent}>{taskName}</span>
+        </li>
+
+    );
+
+
+};
